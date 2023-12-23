d269b81edf6dc0dc8b36aed0a7f4a7b596d1afae
Todd Spencer <spencertodd8@gmail.com>
2023-12-23 11:02:09 -0500

    updated main page

--- script.js
+++
@@ -1,3 +0,0 @@
-// JavaScript code can be added to enhance interactivity.
-// For simplicity, let's assume no additional interactivity is needed for this example.
-// You can add more functionality as per your requirements.
